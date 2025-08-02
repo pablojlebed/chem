56b6e983019fa7876428e18559f75959b1d4ff76
Pablo J Lebed <contact@pablolebed.dev>
2025-08-03 01:12:48 +0200

    code improvs

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,15 @@
 // src/content/config.ts
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 // Define a 'notes' collection schema
 const notesCollection = defineCollection({
-  type: 'content', // 'content' for Markdown/MDX, 'data' for JSON/YAML
+  type: "content", // 'content' for Markdown/MDX, 'data' for JSON/YAML
   schema: z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.date(), // Publication date
     updatedDate: z.date().optional(), // Optional update date
-    tags: z.array(z.string()).default(['technical']), // Tags for filtering/categorization
+    tags: z.array(z.string()).default(["technical"]), // Tags for filtering/categorization
     heroImage: z.string().optional(),
     author: z.string(), // Optional hero image for the note
   }),
@@ -17,5 +17,5 @@ const notesCollection = defineCollection({
 
 // Export your collection(s) here
 export const collections = {
-  'notes': notesCollection,
+  notes: notesCollection,
 };
